dc1b37b09f6e807ed18a9b9a4d9d4bd64032b210
DmitriyIvanovIT <strela199720@gmail.com>
2020-11-01 14:40:10 +0300

    modal window

--- script/Components/openModal.js
+++ script/Components/openModal.js
@@ -2,12 +2,14 @@ const openModal = () => {
     const modal = document.querySelector('.modal');
 
     modal.classList.remove('hide');
+    document.body.classList.add('lock');
 
     modal.addEventListener('click', e => {
         const target = e.target;
 
         if (target === modal || target.closest('.modal__close')) {
             modal.classList.add('hide');
+            document.body.classList.remove('lock');
         }
     });
 };
